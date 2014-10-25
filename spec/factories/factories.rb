1879ee3c06fde0e152beab4e274a97bf77812f81
Mark Chavez <markchav3z@gmail.com>
2014-10-25 12:11:50 +0800

    Add factory for notebook

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,4 +4,10 @@ FactoryGirl.define do
     password { "foobar123" }
     password_confirmation { "foobar123" }
   end
+
+  factory :notebook do
+    name { "Savings" }
+    description { "my savings budget" }
+    user
+  end
 end

668bd0ba25278601dbb1944bfd2e3d289b400423
Mark Chavez <markchav3z@gmail.com>
2014-10-25 11:55:09 +0800

    Basic function to create a notebook record

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -3,9 +3,23 @@ class NotebooksController < ApplicationController
     @notebook = new_notebook
   end
 
+  def create
+    @notebook = Notebook.new(notebook_params)
+    @notebook.save!
+
+    redirect_to root_path, notice: "Notebook created"
+
+  rescue ActiveRecord::RecordInvalid
+    # not implemented yet
+  end
+
   private
 
   def new_notebook
     Notebook.new
+  end
+
+  def notebook_params
+    params.require(:notebook).permit(:name, :description)
   end
 end

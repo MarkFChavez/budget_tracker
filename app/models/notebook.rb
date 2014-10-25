b4ca879eaae84b847dc63d6aeb1fdc8aa16bca1f
Mark Chavez <markchav3z@gmail.com>
2014-10-25 12:24:37 +0800

    Use association as a uniqueness scope not the actual column name

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -2,5 +2,5 @@ class Notebook < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
-  validates :name, uniqueness: { scope: :user_id }
+  validates :name, uniqueness: { scope: :user }
 end

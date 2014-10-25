d4f5017af297db770a3018a03604d4b7a8c7dd82
Mark Chavez <markchav3z@gmail.com>
2014-10-25 12:12:01 +0800

    Add test scenarios for displaying notebooks per user

--- spec/features/creating_a_notebook_spec.rb
+++ spec/features/creating_a_notebook_spec.rb
@@ -3,26 +3,42 @@ require "rails_helper"
 RSpec.describe "Creating a notebook" do
   let(:user) { create(:user) }
 
-  context "signing in" do
+  describe "displaying notebooks" do
     before do
+      @some_user = create(:user)
+      @not_your_notebook = create(:notebook, name: "Not your notebook", user: @some_user)
+      @your_notebook = create(:notebook, name: "Your notebook", user: user)
+
       sign_in_as user
     end
 
-    describe "creating a notebook" do
-      before do
-        click_on "Create notebook"
-
-        fill_in "Name", with: "Credit Card"
-        fill_in "Description", with: "credit card budget"
-        click_on "Save"
+    it "does not show notebooks you did not created" do
+      within("#notebooks") do
+        expect(page).to have_content "Your notebook"
+        expect(page).not_to have_content "Not your notebook"
       end
+    end
+  end
 
-      it "shows it on the dashboard page" do
-        within("#notebooks") do
-          expect(page).to have_content "Credit Card"
-          expect(page).to have_content "credit card budget"
-        end
+  describe "creating a notebook" do
+    before do
+      sign_in_as user
+
+      click_on "Create notebook"
+      fill_in "Name", with: "Credit Card"
+      fill_in "Description", with: "credit card budget"
+      click_on "Save"
+    end
+
+    it "shows it on the dashboard page" do
+      within("#notebooks") do
+        expect(page).to have_content "Credit Card"
+        expect(page).to have_content "credit card budget"
       end
+    end
+
+    it "shows a successful message" do
+      expect(page).to have_content "Notebook created"
     end
   end
 end

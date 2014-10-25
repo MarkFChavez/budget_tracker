fd396a08cf0d2fcf346476bf833f55b511ec0aa1
Mark Chavez <markchav3z@gmail.com>
2014-10-25 12:23:48 +0800

    Add unit tests for notebook (validations)

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,3 +1,6 @@
 class Notebook < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :user_id }
 end
---
+++ spec/models/notebook_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe Notebook do
+  describe "validations" do
+    before do
+      @params = { name: "My notebook", description: "summary of my notebook" }
+    end
+
+    it "requires name" do
+      notebook = Notebook.new(@params.merge(name: ""))
+      expect(notebook).not_to be_valid
+    end
+
+    it "name should should be unique on user scope" do
+      user1 = create(:user)
+      user2 = create(:user)
+      notebook_for_user1 = create(:notebook, name: "Notebook 1", user: user1)
+
+      notebook = Notebook.new(@params.merge(name: "Notebook 1", user: user2))
+      expect(notebook).to be_valid
+    end
+
+    it "does not require description" do
+      notebook = Notebook.new(@params.merge(description: ""))
+      expect(notebook).to be_valid
+    end
+  end
+end
